7e4d1eda5e77ad1bcc36eff0e45ab1a9d2bfd345
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-04-12 21:20:00 +0100

    rework main proc macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,44 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
-use syn::{parse_macro_input, ItemFn};
 use quote::quote;
+
+use lib_voxels_application_core as vxapp_core;
 
 #[proc_macro_attribute]
 pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input_fn = syn::parse_macro_input!(item as syn::ItemFn);
-    let user_block = &input_fn.block;
-    let user_vis = &input_fn.vis;
-    let user_attrs = &input_fn.attrs;
-    let user_sig = &input_fn.sig;
+    let input = syn::parse_macro_input!(item as syn::ItemFn);
+
+    let vis = &input.vis;
+    let attrs = &input.attrs;
+    let sig = &input.sig;
+    let block = &input.block;
+
+    let user_main_fn = quote! {
+        #(#attrs)*
+        #vis fn __voxels_user_main #sig {
+            #block
+        }
+    };
+
+
 
     // Rewrite the function: we generate a real main that loads config and passes it to the user's main
-    let generated = quote! {
-        #(#user_attrs)*
-        #user_vis fn __voxels_user_main #user_sig {
-            #user_block
-        }
-
-        // We replace main() with our own that loads config, sets it, and runs the user's main
+    let wrapper_main = quote! {
         fn main() {
             let config_str = std::fs::read_to_string("voxels.toml")
                 .expect("Could not read voxels.toml");
-            let config: voxels_application_core::VoxelsConfig = toml::from_str(&config_str)
+            let config: vxapp_core::application::Application = toml::from_str(&config_str)
                 .expect("Failed to parse voxels.toml");
             voxels_application_core::set_config(config);
-
             __voxels_user_main();
         }
     };
 
-    generated.into()
+    let output = quote! {
+        #user_main_fn
+        #wrapper_main
+    };
+
+    output.into()
 }
