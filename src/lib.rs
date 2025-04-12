8359c63226b152fa86e26beef1289a2e6ae9f5f0
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-04-12 23:29:47 +0100

    put inserted tokens in their own scope

--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,11 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let wrapped_main = quote! {
         #(#attrs)*
         #vis #sig {
-            let config_str = include_str!("../voxels.toml");
+            let _ = {
+                let config_str = include_str!("../voxels.toml");
 
-            let config : lib_voxels_directories::lib_voxels_application::core::application::Application = toml::from_str(config_str).unwrap();
+                let config : lib_voxels_directories::lib_voxels_application::core::application::Application = toml::from_str(config_str).unwrap();
+            }
 
             #(#stmts)*
         }
