1b835cb7526270f0554ac3f4f57a8ab556ab3e19
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-04-12 21:27:53 +0100

    mke proc macro change wrapped main name

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use proc_macro::TokenStream;
 
 use quote::quote;
 
-use lib_voxels_application_core as vxapp_core;
+use proc_macro2::{Ident, Span};
 
 #[proc_macro_attribute]
 pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -14,24 +14,28 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let sig = &input.sig;
     let block = &input.block;
 
+    let mut new_sig = sig.clone();
+
+    new_sig.ident = Ident::new("__voxels_wrapped_main", Span::call_site());
+
     let user_main_fn = quote! {
         #(#attrs)*
-        #vis fn __voxels_user_main #sig {
+        #vis fn #new_sig {
             #block
         }
     };
-
-
 
     // Rewrite the function: we generate a real main that loads config and passes it to the user's main
     let wrapper_main = quote! {
         fn main() {
             let config_str = std::fs::read_to_string("voxels.toml")
                 .expect("Could not read voxels.toml");
-            let config: vxapp_core::application::Application = toml::from_str(&config_str)
+
+            let config: crate::lib_voxels_application_proc::lib_voxels_application_core::application::Application
                 .expect("Failed to parse voxels.toml");
             voxels_application_core::set_config(config);
-            __voxels_user_main();
+
+            __voxels_wrapped_main();
         }
     };
 
