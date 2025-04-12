25e63109afb63064a0142f910b386b42e9ae8094
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-04-12 23:16:44 +0100

    get file path from env variable

--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,9 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let wrapped_main = quote! {
         #(#attrs)*
         #vis #sig {
-            let config_str = include_str!("voxels.toml");
+            let config_str = concat!(env!("CARGO_MANIFEST_DIR"), include_str!("voxels.toml"));
 
-            let config : lib_voxels_application_core::application::Application = toml::from_str(include_str!("voxels.toml")).unwrap();
+            let config : crate::lib_voxels_application_core::application::Application = toml::from_str(config_str).unwrap();
 
             #(#stmts)*
         }
